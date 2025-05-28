518d96e1be0b22d32e74a6275f4bdf412ef0d80a
gleysi <sonia.mireya.miranda@gmail.com>
2025-05-28 12:23:55 -0600

    TaskForm

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -3,24 +3,52 @@ import "./TaskForm.css";
 import Tag from "./Tag";
 
 const TaskForm = () => {
-  const [taskData, setTaskData] = useState([]);
-  const [task, setTask] = useState("");
-  const [status, setStatus] = useState("todo");
-  const handleTaskChange = (e) => {
-    setTask(e.target.value);
+  const [taskData, setTaskData] = useState({
+    task: "", // the task is the name property of the input field
+    status: "todo", // the status is the name property of the select field
+  });
+
+  // OLD WAY OF HANDLING INPUTS using multiple states
+  // const [task, setTask] = useState("");
+  // const [status, setStatus] = useState("todo");
+  // const handleTaskChange = (e) => {
+  //   setTask(e.target.value);
+  // };
+  // const handleStatusChange = (e) => {
+  //   setStatus(e.target.value);
+  // };
+  // console.log(task, status);
+  ///////////////////////////////
+
+  const handleChange = (e) => {
+    const name = e.target.name; // name of the input or select
+    const value = e.target.value; // value of the input or select
+    // console.log(name, value);
+
+    setTaskData((prev) => {
+      return {
+        // copy all existing properties of taskData using spread operator
+        ...prev,
+        // update the property with the name of the input/select to the new value
+        // name is the name of the input or select (task/status), then we assign a value to the input/select (task/status)
+        [name]: value,
+      };
+    });
   };
-  const handleStatusChange = (e) => {
-    setStatus(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(taskData);
   };
-  console.log(task, status);
   return (
     <header className="app_header">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
+          name="task"
           className="task_input"
           placeholder="Enter your task"
-          onChange={handleTaskChange}
+          onChange={handleChange}
         />
         <div className="task_form_bottom_line">
           <div>
@@ -31,10 +59,10 @@ const TaskForm = () => {
           </div>
           <div>
             <select
-              name=""
+              name="status"
               id=""
               className="task_status"
-              onChange={handleStatusChange}
+              onChange={handleChange}
             >
               <option value="todo">To do</option>
               <option value="doing">Doing</option>
